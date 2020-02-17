56867131247a36a7eea8f36f8ff379638c8a3eba
ahertzog <ahertzog@uncc.edu>
2020-02-17 10:32:34 -0500

    fix syntax

--- src/FileRead.hs
+++ src/FileRead.hs
@@ -1,10 +1,7 @@
 import System.IO
 
-readLines :: FilePath -> IO [String]
-readLines :: fmap lines . readFile
-makeInteger :: [String]  -> [Int]
-makeInteger :: map read
-
 main = do
-    content <- readLines "statNumbers.txt"
-    return (radix $ makeInteger content)
+    handle <- openFile "statsNumbers.txt" ReadMode
+    contents <- hGetContents handle
+    putStr contents
+    hClose handle
