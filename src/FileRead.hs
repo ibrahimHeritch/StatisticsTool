f457ad1185ad128a32534221425d759acaebec7d
Alex Hertzog <ahertzog@uncc.edu>
2020-02-17 16:07:50 -0500

    fixed i think

--- src/FileRead.hs
+++ src/FileRead.hs
@@ -1,14 +1,18 @@
 import System.IO
+import Data.List
 import Data.List.Split
 import Data.Typeable
+import Control.Monad
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
 
 
-readFloat :: String -> IO Float
+readFloat :: String -> Float
 readFloat = read
 
-getInput :: FilePath -> [Float]
-getInput input = do
-    contents <- readFile "statsNumbers.txt" 
-    l <- sequence contents 
-    map readFloat . words $ l
-    
+readContents :: IO()
+readContents = do
+    contents <- readFile "statsNumbers.txt"
+    let x = map readFloat . words $ contents
+    print (show (typeOf x))
+
